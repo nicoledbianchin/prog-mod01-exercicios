ef782fb8eb82d011488a83b4e7dd361a23d5850b
nicoledbianchin <nicoledbianchin@gmail.com>
2022-10-19 20:19:28 -0300

    m5 exercicios frutas

---
+++ m5_exercicios/src/Frutas/Melancia.java
@@ -0,0 +1,32 @@
+package Frutas;
+
+public class Melancia extends Fruta{
+    private double precoAdicional;
+    private boolean epoca;
+
+    public Melancia(String nome, double preco, double precoAdicional, boolean epoca) {
+        super(nome, preco);
+        this.precoAdicional = precoAdicional;
+        this.epoca = epoca;
+    }
+
+    public double getPrecoAdicional() {
+        return precoAdicional;
+    }
+
+    public void setPrecoAdicional(double precoAdicional) {
+        this.precoAdicional = precoAdicional;
+    }
+
+    public boolean isEpoca() {
+        return epoca;
+    }
+
+    public void setEpoca(boolean epoca) {
+        this.epoca = epoca;
+    }
+
+    public double calculaPrecoFinal(){
+        return epoca ? getPreco() : getPreco() + precoAdicional;
+    }
+}
